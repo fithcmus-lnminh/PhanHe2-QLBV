97223015e8c99eb0dfde2dea3775c4490efd0fcc
NTL2503 <ngoloc2706@gmail.com>
2022-06-18 17:29:38 +0700

    TC6_MaHoa

--- Script/Encrypt.sql
+++ Script/Encrypt.sql
@@ -1,8 +1,25 @@
 --Ham ma hoa
-
-
-
-
-
+create or replace function ENCRYPT_DATA(P_DATA varchar2, P_KEY varchar2 default 'NH18_563_566_581')
+return varchar2
+is   
+    V_ENCRYPTED_RAW RAW(2048);
+    V_ENCRYPTION_TYPE  PLS_INTEGER := DBMS_CRYPTO.ENCRYPT_AES128 + DBMS_CRYPTO.CHAIN_CBC + DBMS_CRYPTO.PAD_PKCS5;     
+begin
+  V_ENCRYPTED_RAW := DBMS_CRYPTO.ENCRYPT(SRC => UTL_I18N.STRING_TO_RAW(P_DATA, 'AL32UTF8'), 
+                                         KEY => UTL_I18N.STRING_TO_RAW(P_KEY, 'AL32UTF8'),
+                                         TYP => V_ENCRYPTION_TYPE);
+  return RAWTOHEX(V_ENCRYPTED_RAW);
+end;
 
 --Ham giai ma
+create or replace function DECRYPT_DATA(P_DATA RAW, P_KEY varchar2 default 'NH18_563_566_581')
+return varchar2
+is 
+    V_DECRYPTED_RAW RAW(2048);
+    V_ENCRYPTION_TYPE  PLS_INTEGER := DBMS_CRYPTO.ENCRYPT_AES128 + DBMS_CRYPTO.CHAIN_CBC + DBMS_CRYPTO.PAD_PKCS5;       
+begin
+  V_DECRYPTED_RAW := DBMS_CRYPTO.DECRYPT(SRC => HEXTORAW(P_DATA), 
+                                         KEY => UTL_I18N.STRING_TO_RAW(P_KEY, 'AL32UTF8'),
+                                         TYP => V_ENCRYPTION_TYPE);
+  return UTL_I18N.RAW_TO_CHAR(V_DECRYPTED_RAW);
+end;
---
+++ Script/TC6.sql
@@ -0,0 +1,67 @@
+--View Table Nhan Vien
+create or replace function ViewNhanVien(P_Schema varchar2, P_Object varchar2)
+return varchar2
+as
+    usr varchar2(100);
+    vt varchar2(100);
+begin
+    usr := sys_context('USERENV', 'SESSION_USER');
+    if (usr like '%NV%') then
+        select VAITRO into vt from QLCSYT.NHANVIEN where MANV = usr;
+        if (vt = '') then 
+            vt := 'A';
+        end if;
+    end if;
+    if (usr like 'QLCSYT') then 
+        usr := '';
+    end if;
+    if (vt like 'Thanh tra') then 
+        usr := '';
+    end if;
+    return 'MANV LIKE ''%' || usr || '''';
+end;
+
+begin
+    sys.dbms_rls.add_policy (
+        object_schema => 'QLCSYT',
+        object_name => 'NHANVIEN',
+        policy_name => 'ViewNhanVien',
+        policy_function => 'ViewNhanVien',
+        statement_types => 'select, update',
+        update_check => TRUE
+    );
+end;
+
+--View Table Benh Nhan
+create or replace function ViewBenhNhan(P_Schema varchar2, P_Object varchar2)
+return varchar2
+as
+    usr varchar2(100);
+    vt varchar2(100);
+begin
+    usr := sys_context('USERENV', 'SESSION_USER');
+    if (usr like '%NV%') then
+        select VAITRO into vt from QLCSYT.NHANVIEN where MANV = usr;
+        if (vt = '') then 
+            vt := 'A';
+        end if;
+    end if;
+    if (usr like 'QLCSYT') then 
+        usr := '';
+    end if;
+    if (vt like 'Thanh tra') then 
+        usr := '';
+    end if;
+    return 'MABN LIKE ''%' || usr || '''';
+end;
+
+begin
+    sys.dbms_rls.add_policy (
+        object_schema => 'QLCSYT',
+        object_name => 'BENHNHAN',
+        policy_name => 'ViewBenhNhan',
+        policy_function => 'ViewBenhNhan',
+        statement_types => 'select, update',
+        update_check => TRUE
+    );
+end;
